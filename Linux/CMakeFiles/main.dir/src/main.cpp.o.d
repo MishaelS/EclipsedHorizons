8a529789e838cd19a60804bafe58de3b694495ad
Amirka04 <kokovamir6@gmail.com>
2025-01-11 10:58:23 +0300

    Удаление директорий с сборкой

--- Linux/CMakeFiles/main.dir/src/main.cpp.o.d
+++
@@ -1,5 +0,0 @@
-CMakeFiles/main.dir/src/main.cpp.o: \
- /home/cyberamirka/Загрузки/Telegram\ Desktop/PixelSurvivor_ForgottenLands/ForgottenLands/src/main.cpp \
- /usr/include/stdc-predef.h \
- /home/cyberamirka/Загрузки/Telegram\ Desktop/PixelSurvivor_ForgottenLands/ForgottenLands/lib/raylib-master/src/raylib.h \
- /usr/lib/gcc/x86_64-redhat-linux/14/include/stdarg.h
--- build/CMakeFiles/EclipsedHorizons.dir/src/utils/globals.cpp.obj.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/EclipsedHorizons.dir/src/utils/globals.cpp.obj: \
- C:\Dev\projectsCpp\EclipsedHorizons\src\utils\globals.cpp \
- C:/Dev/projectsCpp/EclipsedHorizons/include/utils/globals.hpp
